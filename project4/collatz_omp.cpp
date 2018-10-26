f68e633c5d20d5c39f4d6b8a330e0c6411ff652c
Halimah Mohammed <halimah@Halimahs-MacBook-Pro.local>
2018-10-26 17:47:28 -0500

    nsss

--- project4/collatz_omp.cpp
+++ project4/collatz_omp.cpp
@@ -30,7 +30,7 @@ static int collatz(const long range, long thread_count)
 {
   // compute sequence lengths
   int maxlen = 0;
-  #pragma omp parallel for num_threads(thread_count) default(none) shared(maxlen, thread_count) SCHED(static, 1)
+  #pragma omp parallel for num_threads(thread_count) default(none) shared(maxlen, thread_count)
   for (long i = 1; i <= range; i++) {
     long val = i;
     int len = 1;
