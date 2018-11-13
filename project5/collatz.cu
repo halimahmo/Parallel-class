56d9f079856c72779fc4c60230c03fabbbdcfe2f
Halimah Mohammed <halimah@Halimahs-MacBook-Pro.local>
2018-11-12 20:12:32 -0600

    funn

--- project5/collatz.cu
+++ project5/collatz.cu
@@ -29,7 +29,7 @@ Author: Martin Burtscher
 
 static const int ThreadsPerBlock = 512;
 
-static __global__ void collatzKernel(int* maxlen, const long range,)
+static __global__ void collatzKernel(int* maxlen, const long range)
 {
   // compute sequence lengths
   const long idx = threadIdx.x + blockIdx.x * (long)blockDim.x;
