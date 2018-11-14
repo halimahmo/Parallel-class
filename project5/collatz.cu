e3882e53b1c8c7960f32ee222f6c03ecc1cd0750
Halimah Mohammed <halimah@Halimahs-MacBook-Pro.local>
2018-11-14 15:21:00 -0600

    cvc

--- project5/collatz.cu
+++ project5/collatz.cu
@@ -49,7 +49,6 @@ static __global__ void  collatzKernel(const long range, int* maxlen)
 
     // if (newMaxlen < len) newMaxlen = len;
     //   *maxlen = atomicMax(newMaxlen, len);
-      
     //thread updating maxlen using atomicMax
     if (*maxlen < len) *maxlen = atomicMax(maxlen, len);
   }
