5c8e0b8e87f44fc21df1620b7453e733855a64f9
Halimah Mohammed <halimah@Halimahs-MacBook-Pro.local>
2018-12-04 23:59:46 -0600

    chng2

--- finalProject/fractal_hyb.cu
+++ finalProject/fractal_hyb.cu
@@ -36,6 +36,33 @@ static const double yMid =  0.55267;
 static __global__ void FractalKernel(const int start_frame, const int gpu_frames, const int width, unsigned char* pic_d)
 {
   // todo: use the GPU to compute the frames (base the code on the previous project)
+  // compute frames
+  const long idx = threadIdx.x + blockIdx.x * (long)blockDim.x;
+  if(idx < n){
+  const int frame = idx / (width * width);
+    const double delta = Delta * pow(0.98, frame);
+    const double xMin = xMid - delta;
+    const double yMin = yMid - delta;
+    const double dw = 2.0 * delta / width;
+
+    const int row = (idx / width) % width; 
+      const double cy = yMin + row * dw;
+
+      const int col = idx % width;
+        const double cx = xMin + col * dw;
+        double x = cx;
+        double y = cy;
+        int depth = 256;
+        double x2, y2;
+        do {
+          x2 = x * x;
+          y2 = y * y;
+          y = 2 * x * y + cy;
+          x = x2 - y2 + cx;
+          depth--;
+        } while ((depth > 0) && ((x2 + y2) < 5.0));
+        pic[frame * width * width + row * width + col] = (unsigned char)depth;
+      }
 }
 
 unsigned char* GPU_Init(const int gpu_frames, const int width)
@@ -48,9 +75,14 @@ unsigned char* GPU_Init(const int gpu_frames, const int width)
 void GPU_Exec(const int start_frame, const int gpu_frames, const int width, unsigned char* pic_d)
 {
   // todo: launch the kernel with ThreadsPerBlock and the appropriate number of blocks
+  FractalKernel<<<(N + ThreadsPerBlock - 1) / ThreadsPerBlock, ThreadsPerBlock>>>(width, frames, d_pic, N);
+  //cudaDeviceSynchronize();
 }
 
 void GPU_Fini(const int gpu_frames, const int width, unsigned char* pic, unsigned char* pic_d)
 {
   // todo: copy the result from the device to the host and free the device memory
+  if (cudaSuccess != cudaMemcpy(pic, d_pic, size, cudaMemcpyDeviceToHost)) {fprintf(stderr, "copying from device failed\n"); exit(-1);}
+  
+  cudaFree(d_pic);
 }
